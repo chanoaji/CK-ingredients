4a746c79a1b64c660129dc4f6d502d1e18523610
Vanessa Pasquet <pasquet.vanessa@gmail.com>
2013-05-15 14:32:50 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CKIngredients::Application.config.secret_token = 'ef519c252aac4cce86d9de91194b0f5af5d477e5be1168676f4fa62aba35dd6edbcf367b1f277349eeb940a0f9c4e3291112c5ddddb19d072ad908b714fc4a10'
